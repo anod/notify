4e2aac5c5200801374a0d797a021de21bbaf8822
Alexandr Gavrishev <algavris@microsoft.com>
2020-12-02 17:54:06 +0200

    Command line app to show notification for Win

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.CommandLine;
+using Windows.UI.Notifications;
+using System.CommandLine.Invocation;
+using System.IO;
+
+namespace notify
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+           var rootCommand = new RootCommand("Notify")
+            {
+                new Option<string>(
+                    new string[] { "--app-id", "-a" },
+                    description: "Application identifier"
+                ) {
+                    IsRequired = true
+                },
+                new Option<string>(
+                    new string[] { "--title", "-t" },
+                    "Notification title"
+                ) {
+                    IsRequired = true
+                },
+                new Option<string>(
+                    new string[] { "--body", "-b" },
+                    "Notification body"
+                ) {
+                    IsRequired = true
+                },
+                new Option<FileInfo>(
+                    new string[] { "--image", "-i" },
+                    "Notification image path"
+                ) {
+                    Argument = new Argument<FileInfo>().ExistingOnly()
+                }
+            };
+
+            // Note that the parameters of the handler method are matched according to the names of the options
+            rootCommand.Handler = CommandHandler.Create(
+                (string appId,string title,string body, FileInfo image) =>
+            {
+                Show(appId, title, body, image?.FullName);
+                Thread.Sleep(300);
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
+        }
+
+        private static void Show(string appId, string title, string body, string imagePath)
+        {
+            
+            var templateType = (imagePath == null) ? ToastTemplateType.ToastText02 : ToastTemplateType.ToastImageAndText02;
+            var template = ToastNotificationManager.GetTemplateContent(templateType);
+
+            if (imagePath != null) {
+                var images = template.GetElementsByTagName("image");
+                var src = template.CreateAttribute("src");
+                src.Value = imagePath;
+                images.Item(0).Attributes.SetNamedItem(src);
+            }
+
+            var textNodes = template.GetElementsByTagName("text");
+            textNodes.Item(0).InnerText = title;
+            textNodes.Item(1).InnerText = body;
+
+            var notifier = ToastNotificationManager.CreateToastNotifier(appId);
+            var notification = new ToastNotification(template);
+            notifier.Show(notification);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Notify - Show notification from command line
+====
+
+.NET 5.0 + C#/WinRT
+
+=== Usage
+
+dotnet .\bin\Release\net5.0-windows10.0.19041.0\win10-x64\notify.dll
